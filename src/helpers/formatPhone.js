ee16427c62415f92f26a6711f76e4401923fbbcd
umair <umair@getbee.com>
2024-08-29 15:39:59 +0500

    fix phone format check

--- src/helpers/formatPhone.js
+++ src/helpers/formatPhone.js
@@ -1,7 +1,7 @@
 export default (phoneNumber) => {
   let phone = `${phoneNumber.replace(/\s/g, "")}`;
   let number = phone.match(/(3[0-9]*)/g);
-  return Array.isArray(number) && number[0].length < 10
+  return Array.isArray(number) && number[0].length >= 10
     ? number[0]
     : phoneNumber;
 };
