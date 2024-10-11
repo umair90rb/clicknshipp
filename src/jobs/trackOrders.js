2917e2ea95a2cdc5711d1691c45b01f72a22d5b6
umair <umair@getbee.com>
2024-10-11 22:12:55 +0500

    add limit on delivery tracking

--- src/jobs/trackOrders.js
+++ src/jobs/trackOrders.js
@@ -41,6 +41,8 @@ schedule(every15Sec, async () => {
         },
       },
       attributes: ['id', 'cn', 'account_id'],
+      order: [['createdAt', 'DESC']],
+      limit: 10,
     });
     console.log(`No of orders for tracking: ${deliveriesToTrack.length}`);
     if (!deliveriesToTrack?.length) {
