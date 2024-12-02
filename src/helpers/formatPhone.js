410c3bf6e86411987ac7e55628beac13d96aa8ed
umair <umair@getbee.com>
2024-12-02 14:39:21 +0500

    covert number to string in formatPhone.js

--- src/helpers/formatPhone.js
+++ src/helpers/formatPhone.js
@@ -1,5 +1,5 @@
 export default (phoneNumber) => {
-  let phone = `${phoneNumber.replace(/\s/g, "")}`;
+  let phone = `${String(phoneNumber).replace(/\s/g, '')}`;
   let number = phone.match(/(3[0-9]*)/g);
   return Array.isArray(number) && number[0].length >= 10
     ? number[0]
