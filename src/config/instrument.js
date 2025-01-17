d3c8a8c932d12a97ef54e053a48071eabcc6b570
umair <umair@getbee.com>
2025-01-17 11:17:51 +0500

    update sentry

--- src/config/instrument.js
+++ src/config/instrument.js
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/node';
 
 Sentry.init({
-  dsn: 'https://10691e6c9d3bedaef60a00929419154e@o4507859402096640.ingest.us.sentry.io/4507859406094336',
+  dsn: process.env.SENTRY_DNS,
 });
